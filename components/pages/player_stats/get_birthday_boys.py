2b406413d512580cb27db3a95cbb4a911351ad0a
Thomas <thomasleblay98@gmail.com>
2025-07-16 14:33:25 +0200

    fix: Player Stats: birthday boys with club. in 5 previous seasons. reformatted.

--- components/pages/player_stats/get_birthday_boys.py
+++ components/pages/player_stats/get_birthday_boys.py
@@ -6,11 +6,14 @@ from components.queries.execute_query import execute_query
 from utils.file_helper.reader import read_sql_file
 
 
-@st.cache_data(show_spinner=False)
+def format_dict(d):
+    return ", ".join(f"{club}: [{', '.join(seasons)}]" for club, seasons in d.items())
+
+# @st.cache_data(show_spinner=False)
 def get_birthday_boys_by_season(_db_conn, season_schema):
     sql_file = read_sql_file(
         file_name=f"components/queries/player_stats/birthday_boys.sql",
-        season_schema=season_schema
+        season=season_schema[7:]
     )
     return execute_query(_db_conn, sql_file)
 
@@ -24,4 +27,20 @@ def get_birthday_boys(db_conn):
         df_season = get_birthday_boys_by_season(db_conn, season_schema)
         df = pd.concat([df, df_season], ignore_index=True)
 
-    st.dataframe(df.drop_duplicates(), hide_index=True)
+    df = df
+    season_club_dict = (
+        df.groupby(["Id", "Player", "Age"])
+        .apply(lambda x: x.groupby("Club")["Season"].apply(list).to_dict())
+    )
+
+    df["Season x Club"] = (df
+                           .set_index(["Id", "Player", "Age"])
+                           .index.map(season_club_dict)
+    )
+    df["Season x Club"] = df["Season x Club"].apply(format_dict)
+
+    df = df.drop(columns=["Season", "Club"])
+    df = df.drop_duplicates(subset=["Id", "Player"])
+
+    st.dataframe(df.sort_values(["Age", "Player"]), hide_index=True, use_container_width=True)
+    # st.markdown(render_html_table(df), unsafe_allow_html=True)
--- components/queries/player_stats/birthday_boys.sql
+++ components/queries/player_stats/birthday_boys.sql
@@ -1,15 +1,16 @@
 select distinct
+    p.id as "Id",
     p.name as "Player",
     p.birth_country as "Birth country",
-    c.name as "Club",
-    EXTRACT(YEAR FROM AGE(current_date, p.birth_date)) as "Age"
-from {{ season_schema }}.team_player tp
+    EXTRACT(YEAR FROM AGE(current_date, p.birth_date)) as "Age",
+    '{{ season }}' as "Season",
+    c.name as "Club"
+from season_{{ season }}.team_player tp
 left join upper.player p
 on tp.player = p.id
-left join {{ season_schema }}.team t
+left join season_{{ season }}.team t
 on tp.team = t.id
 left join upper.club c
 on t.club = c.id
 where extract(month from birth_date) = extract(month from current_date)
-and extract(day from birth_date) = extract(day from current_date)
-order by "Age";
+and extract(day from birth_date) = extract(day from current_date);
