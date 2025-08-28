068f1a5fc772b03f0500dc23935c0111968e32a3
Thomas <thomasleblay98@gmail.com>
2025-08-28 11:03:57 +0200

    feat: home_away balance with goals and cards.

--- components/pages/team_stats/all_competitions/get_home_away_outcomes.py
+++ components/pages/team_stats/all_competitions/get_home_away_outcomes.py
@@ -15,7 +15,7 @@ def get_balance(_db_conn):
     return execute_query(_db_conn, sql_file)
 
 
-def compute_ratio(row):
+def compute_goals_ratio(row):
     if row["Side"] == "Home Goals":
         return row["% Home Goals"]*100
     elif row["Side"] == "Away Goals":
@@ -35,93 +35,34 @@ def get_home_away_balance(db_conn):
 
     set_sub_sub_sub_title("All seasons")
     df_all_seasons = df[df["Season"] == "All"]
-    get_home_away_outcomes_all_seasons_plotly(df_all_seasons)
+    get_home_away_balance_all_seasons_plotly(df_all_seasons)
 
     colors_by_comp = set_colors_by_comp()
     df_by_season = df[df["Season"] != "All"]
     df_by_season = df_by_season.sort_values(["Season", "Competition"])
 
-    set_sub_sub_sub_title("Goals by season")
-    side_goals = st.radio(
+    set_sub_sub_sub_title("Stats by season")
+
+    col, _ = st.columns(2)
+    with col:
+        chosen_stat = st.selectbox(
+            key='home_away_balance__stat',
+            label="Choose stat",
+            options=["Goals", "Yellow Cards", "2nd Yellow Cards", "Red Cards"]
+        )
+
+    side = st.radio(
         key='home_away_balance__side_goals',
         label="Side",
-        options=["Home Goals", "Total Goals", "Away Goals"],
+        options=["Home", "Both", "Away"],
         horizontal=True,
         label_visibility="collapsed",
         index=1
     )
-    get_home_away_goals_by_season_plotly(df_by_season, side_goals, colors_by_comp)
-
-    set_sub_sub_sub_title("Outcomes by season")
-    side_outcomes = st.radio(
-        key='home_away_balance__side_outcomes',
-        label="Side",
-        options=["Home Wins", "Draws", "Away Wins"],
-        horizontal=True,
-        label_visibility="collapsed",
-        index=0
-    )
-    get_home_away_outcomes_by_season_plotly(df_by_season, side_outcomes, colors_by_comp)
+    get_home_away_stats_by_season_plotly(df_by_season, chosen_stat, side, colors_by_comp)
 
 
-def get_home_away_outcomes(df):
-
-    df_outcomes_melt = df.melt(
-        id_vars=["Competition", "Matches"],
-        value_vars=["Home Wins", "Draws", "Away Wins"],
-        var_name="Side",
-        value_name="Count"
-    )
-    df_outcomes_melt['Side Light'] = df_outcomes_melt['Side'].str.replace(' Wins', '')
-    df_outcomes_melt['Ratio'] = (df_outcomes_melt['Count'] / df_outcomes_melt['Matches']) * 100
-
-    df_goals_melt = df.melt(
-        id_vars=["Competition", "Matches"],
-        value_vars=["Home Goals", "Total Goals", "Away Goals"],
-        var_name="Side",
-        value_name="Count"
-    )
-    df_goals_melt['Side Light'] = df_goals_melt['Side'].str.replace(' Goals', '')
-    df_goals_melt['Avg'] = (df_goals_melt['Count'] / df_goals_melt['Matches'])
-
-    outcome_chart = alt.Chart(df_outcomes_melt).mark_bar().encode(
-        x=alt.X("Competition:N"),
-        y=alt.Y('Count:Q', title='#'),
-        xOffset=alt.XOffset('Side Light:N', sort=['Home', 'Draws', 'Away']),
-        color=alt.Color(
-            "Side:N",
-            scale=alt.Scale(
-                domain=["Home Wins", "Draws", "Away Wins", "Home Goals", "Total Goals", "Away Goals"],
-                range=['#1f77b4', '#aec7e8', '#1f77b4', 'orange', 'firebrick', 'orange']
-            ),
-            sort=['Home Wins', 'Draws', 'Away Wins', "Home Goals", "Total Goals", "Away Goals"],
-            legend=alt.Legend(title='Outcome and Goals')
-        ),
-        tooltip=["Competition", "Side", "Count:Q", alt.Tooltip("Ratio:Q", format=".2f", title="Ratio (%)")]
-    )
-
-    goals_chart = alt.Chart(df_goals_melt).mark_bar().encode(
-        x=alt.X("Competition:N"),
-        y=alt.Y('Count:Q', title='#'),
-        xOffset=alt.XOffset('Side Light:N', sort=['Home', 'Draws', 'Away']),
-        color=alt.Color(
-            "Side:N",
-            scale=alt.Scale(
-                domain=["Home Wins", "Draws", "Away Wins", "Home Goals", "Total Goals", "Away Goals"],
-                range=['#1f77b4', '#aec7e8', '#1f77b4', 'orange', 'firebrick', 'orange']
-            )
-        ),
-        tooltip=["Competition", "Side", "Count:Q", alt.Tooltip("Avg:Q", format=".2f", title="Per match")]
-    )
-
-    chart = (goals_chart + outcome_chart).properties(
-        title="Match Outcomes and Goals by Competition in the 21th century",
-        height=600
-    )
-
-    st.altair_chart(chart, use_container_width=True)
-
-def get_home_away_outcomes_all_seasons_plotly(df):
+def get_home_away_balance_all_seasons_plotly(df):
     color_map_outcomes = {
         "Home Wins": '#1f77b4',
         "Draws": '#aec7e8',
@@ -149,7 +90,7 @@ def get_home_away_outcomes_all_seasons_plotly(df):
         value_name="Count"
     )
     df_goals_melt['Avg'] = (df_goals_melt['Count'] / df_goals_melt['Matches'])
-    df_goals_melt['Ratio'] = df_goals_melt.apply(compute_ratio, axis=1)
+    df_goals_melt['Ratio'] = df_goals_melt.apply(compute_goals_ratio, axis=1)
     df_goals_melt["Color"] = df_goals_melt["Side"].map(color_map_goals)
 
     # Graphique outcomes
@@ -203,54 +144,25 @@ def get_home_away_outcomes_all_seasons_plotly(df):
     with col2:
         st.plotly_chart(fig_goals, use_container_width=True)
 
-
-def get_home_away_goals_by_season_plotly(df, side, colors):
-    df["Goals / Match"] = df[side] / df["Matches"]
+def get_home_away_stats_by_season_plotly(df, stat, side, colors):
+    side_stat = f"{side} {stat}" if side != 'Both' else f"Total {stat}"
+    df[f"{side_stat} / Match"] = df[side_stat] / df["Matches"]
 
     fig = px.line(
         df,
         x='Season',
-        y='Goals / Match',
+        y=f'{side_stat} / Match',
         color='Competition',
         color_discrete_map=colors,
-        title=f"{"" if side == 'Both' else f'{side} '}Goals per Match"
+        title=f"{side_stat} per Match"
     )
 
     fig.update_traces(
         hovertemplate=(
                 "<b>Competition: </b>%{fullData.name}<br>" +
                 "<b>Season: </b>%{x}<br>" +
-                f"<b>Side: </b>{side}<br><br>" +
-                "<b>Goals / Match: </b>%{y:.2f}" +
-                "<extra></extra>"
-        )
-    )
-    st.plotly_chart(fig, use_container_width=True)
-
-    csv = df.to_csv(index=False, sep='|')
-    st.download_button(
-        label="📥 Download CSV",
-        data=csv,
-        file_name=f"all_competitions_{side.lower()}_stats_by_season.csv",
-        mime="text/csv"
-    )
-
-def get_home_away_outcomes_by_season_plotly(df, side, colors):
-    fig = px.line(
-        df,
-        x='Season',
-        y=side,
-        color='Competition',
-        color_discrete_map=colors,
-        title=side
-    )
-
-    fig.update_traces(
-        hovertemplate=(
-                "<b>Competition: </b>%{fullData.name}<br>" +
-                "<b>Season: </b>%{x}<br>" +
-                f"<b>Side: </b>{side}<br><br>" +
-                f"<b>Outcome: </b>%{{y}}" +
+                f"<b>Side: </b>{side_stat}<br><br>" +
+                f"<b>{stat} / Match: </b>%{{y:.2f}}" +
                 "<extra></extra>"
         )
     )
--- components/queries/team_stats/all_competitions/balance/get_home_away_balance.sql
+++ components/queries/team_stats/all_competitions/balance/get_home_away_balance.sql
@@ -2,11 +2,23 @@ with sums as (
     select
         case when grouping(season) = 1 then 'All' else season end as "Season",
         competition as "Competition",
+
         sum(home_win) as "Home Wins",
         sum(home_draw) as "Draws",
         sum(away_win) as "Away Wins",
+
         sum(case when played_home then home_score end) as "Home Goals",
         sum(case when not played_home then away_score end) as "Away Goals",
+
+        sum(case when played_home then home_y_cards end) as "Home Yellow Cards",
+        sum(case when not played_home then away_y_cards end) as "Away Yellow Cards",
+
+        sum(case when played_home then home_yr_cards end) as "Home 2nd Yellow Cards",
+        sum(case when not played_home then away_yr_cards end) as "Away 2nd Yellow Cards",
+
+        sum(case when played_home then home_r_cards end) as "Home Red Cards",
+        sum(case when not played_home then away_r_cards end) as "Away Red Cards",
+
         sum(home_match) as "Matches"
     from analytics.staging_teams_performance stp
     where season >= '2000-2001' and (round is null or round != 'Final')
@@ -15,14 +27,67 @@ with sums as (
 select
     "Season",
     "Competition",
+
     "Home Wins",
     "Draws",
     "Away Wins",
+
     "Home Goals",
-    "Home Goals" / ("Home Goals" + "Away Goals")::numeric as "% Home Goals",
-    "Home Goals" + "Away Goals" as "Total Goals",
-    "Away Goals" / ("Home Goals" + "Away Goals")::numeric as "% Away Goals",
     "Away Goals",
+    case
+        when "Home Goals" + "Away Goals" = 0
+        then 0
+        else "Home Goals" / ("Home Goals" + "Away Goals")::numeric
+    end as "% Home Goals",
+    "Home Goals" + "Away Goals" as "Total Goals",
+    case
+        when "Home Goals" + "Away Goals" = 0
+        then 0
+        else "Away Goals" / ("Home Goals" + "Away Goals")::numeric
+    end as "% Away Goals",
+
+    "Home Yellow Cards",
+    "Away Yellow Cards",
+    case
+        when "Home Yellow Cards" + "Away Yellow Cards" = 0
+        then 0
+        else "Home Yellow Cards" / ("Home Yellow Cards" + "Away Yellow Cards")::numeric
+    end "% Home Yellow Cards",
+    case
+        when "Home Yellow Cards" + "Away Yellow Cards" = 0
+        then 0
+        else "Away Yellow Cards" / ("Home Yellow Cards" + "Away Yellow Cards")::numeric
+    end "% Away Yellow Cards",
+    "Home Yellow Cards" + "Away Yellow Cards" as "Total Yellow Cards",
+
+    "Home 2nd Yellow Cards",
+    "Away 2nd Yellow Cards",
+    case
+        when "Home 2nd Yellow Cards" + "Away 2nd Yellow Cards" = 0
+        then 0
+        else "Home 2nd Yellow Cards" / ("Home 2nd Yellow Cards" + "Away 2nd Yellow Cards")::numeric
+    end as "% Home 2nd Yellow Cards",
+    case
+        when "Home 2nd Yellow Cards" + "Away 2nd Yellow Cards" = 0
+        then 0
+        else "Away 2nd Yellow Cards" / ("Home 2nd Yellow Cards" + "Away 2nd Yellow Cards")::numeric
+    end as "% Away 2nd Yellow Cards",
+    "Home 2nd Yellow Cards" + "Away 2nd Yellow Cards" as "Total 2nd Yellow Cards",
+
+    "Home Red Cards",
+    "Away Red Cards",
+    case
+        when "Home Red Cards" + "Away Red Cards" = 0
+        then 0
+        else "Home Red Cards" / ("Home Red Cards" + "Away Red Cards")::numeric
+    end as "% Home Red Cards",
+    case
+        when "Home Red Cards" + "Away Red Cards" = 0
+        then 0
+        else "Away Red Cards" / ("Home Red Cards" + "Away Red Cards")::numeric
+    end as "% Away Red Cards",
+    "Home Red Cards" + "Away Red Cards" as "Total Red Cards",
+
     "Matches"
 from sums
 ;
